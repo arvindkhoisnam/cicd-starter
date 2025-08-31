975d546491699dc1f8a304111def661252169496
arvindkhoisnam <khoisnamarvind@gmail.com>
2025-09-01 00:08:58 +0530

    testing-ci-test

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,32 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+type header struct{
+}
+
+func(h *header)Get(key string)string{
+	return "ApiKey itisamockapikey"
+}
+
+
+func TestGetAPIKey(t *testing.T){
+
+	apiKey := "ApiKey this_is_a_temporary_api_key"
+	expect := "this_is_a_temporary_api_key"
+	header := make(http.Header)
+	header.Set("Authorization",apiKey)
+
+	res,err := GetAPIKey(header)
+	if err != nil{
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	if res != expect{
+		t.Errorf("expected %q, got %q", expect, res)
+	}
+}
+
